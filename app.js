e28ce5c1ab9b7d412c589c8d38a8bdd078fe5228
EUIJONE LEE <krocha072@gmail.com>
2018-07-18 11:51:28 +0900

    게시판 작업

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
 
 //pug세팅
 app.set('views', './views'); // ./인경우 현재경로
 app.set('view engine', 'pug');
 app.locals.pretty = true;
+
+//post
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//static파일
+app.use(express.static('public'));
 
 var mainer = require('./server/routes/mainController.js');
 app.use('/main', mainer);
--- server/routes/mainController.js
+++ server/routes/mainController.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mainer = express.Router();
+var fs = require('fs'); //파일시스템 기본모듈
 
 mainer.get('/login', function(req, res){
   res.send('로그인');
@@ -15,8 +16,74 @@ mainer.get('/register', (req, res)=>{
 });
 
 mainer.get('/', (req, res)=>{
-  res.send('메인화면');
-
+    res.render('main/boardList');
 });
+
+mainer.get(['/boardList', '/boardList/:title'], (req, res)=>{
+
+  fs.readdir('data', (err, files)=>{
+    if(err)
+    {
+      console.log(err);
+      res.status(500).send('죄송합니다. 서버 측 에러입니다.');
+    }
+    else
+    {
+      var title = req.params.title;
+      //title이 있는가 없는가 체크
+      if(!(title))
+      {
+        res.render('main/boardList', {boardList: files});
+      }
+      else{
+        fs.readFile('data/'+title, 'utf8', (err, data)=>{
+          if(err){
+            console.log(err);
+            res.status(500).send('죄송합니다. 서버 측 에러입니다.');
+          }
+          else {
+            res.render('main/boardList',
+            {
+              title: title,
+              content: data,
+              boardList: files
+            });
+          }
+        });
+      }
+
+    }//else
+
+  });//readdir
+});
+
+mainer.post('/boardList', (req, res)=>{
+    var title = req.body.title; //파일이름
+    var content = req.body.content; //파일내용
+    fs.writeFile(`data/${title}`, content, (error)=>{
+        if(error){
+          console.log(error);
+          res.status(500).send('죄송합니다. 서버 측 에러입니다.');
+        }
+        else {
+          fs.readdir('data', (err, files)=>{
+            if(err)
+            {
+              console.log(err);
+              res.status(500).send('죄송합니다. 서버 측 에러입니다.');
+            }
+            else
+            {
+              res.render('main/boardList', {boardList: files});
+            }
+
+          });
+
+        }
+    });
+});
+
+
+
 
 module.exports = mainer;
