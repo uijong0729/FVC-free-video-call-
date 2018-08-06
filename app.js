7764d37a937e4c7f6bcdd3ed0879b31fe4646f52
uijong <uijong0729@naver.com>
2018-08-06 14:39:01 +0900

    부트스트랩과 제이쿼리

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mainer = require('./server/routes/mainController.js'); //라우터
 const app = express();
 //const dbcon = require('./dbinit.js');
-const dbcon = require('./dbmongooseinit.js');
+//const dbcon = require('./dbmongooseinit.js');
 
 
 //pug세팅
@@ -15,6 +15,14 @@ app.locals.pretty = true;
 app.use(express.static('public'));
 app.use(express.static('uploads'));
 app.use('user', express.static('uploads'));
+
+//부트스트랩 제이쿼리
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+
 
 app.use('/main', mainer); //라우터
 app.use(bodyParser.urlencoded({ extended: false }));  //post
